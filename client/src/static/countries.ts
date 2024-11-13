aed09b0e0762a1ad0686c26726d51bd3b14161d8
Pedro Stanzani <pedrohstanzani@gmail.com>
2024-11-13 12:47:44 -0300

    Memoize visaRequirementsMap

--- client/src/static/countries.ts
+++ client/src/static/countries.ts
@@ -1,20 +1,25 @@
 import pako from "pako";
 import data from "@/static/compressed.json";
+import { useMemo } from "react";
 
 export const visaRequirementsEnum: string[] = data.visa_enum;
 
 const useVisaRequirements = () => {
-  const binaryData = Uint8Array.from(atob(data.compressed), (c) =>
-    c.charCodeAt(0)
-  );
-  const decompressedData = pako.inflate(binaryData, { to: "string" });
+  const visaRequirementsMap = useMemo(() => {
+    const binaryData = Uint8Array.from(atob(data.compressed), (c) =>
+      c.charCodeAt(0)
+    );
+    const decompressedData = pako.inflate(binaryData, { to: "string" });
 
-  const rows = decompressedData.trim().split("\n");
-  const visaRequirementsMap = new Map<string, number>();
-  rows.slice(1).forEach((row) => {
-    const [citizenship, destination, visaRequirement] = row.split(",");
-    visaRequirementsMap.set(`${citizenship}-${destination}`, Number(visaRequirement));
-  });
+    const map = new Map<string, number>();
+    const rows = decompressedData.trim().split("\n");
+    rows.slice(1).forEach((row) => {
+      const [citizenship, destination, visaRequirement] = row.split(",");
+      map.set(`${citizenship}-${destination}`, Number(visaRequirement));
+    });
+
+    return map;
+  }, []);
 
   const getVisaRequirement = (citizenship: string, destination: string) => {
     const key = `${citizenship}-${destination}`;
